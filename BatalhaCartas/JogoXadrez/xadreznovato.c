9ca53d47cf2fb3a36bbec18aa4c4d4a13d819992
Olson <biasolisan@gmail.com>
2025-08-26 12:19:09 +0000

    nivelnovatoxadrez

---
+++ BatalhaCartas/JogoXadrez/xadreznovato.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    // Movimento da Torre (horizontal para a direita usando FOR)
+    int casasTorre = 5;
+    printf("Movimento da Torre (5 casas para a Direita):\n");
+
+    for (int i = 1; i <= casasTorre; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\n");
+
+    // Movimento do Bispo (diagonal para cima e à direita usando WHILE)
+    int casasBispo = 5;
+    int contadorBispo = 1;
+    printf("Movimento do Bispo (5 casas na Diagonal - Cima Direita):\n");
+
+    while (contadorBispo <= casasBispo) {
+        printf("Cima Direita\n");
+        contadorBispo++;
+    }
+
+    printf("\n");
+
+    // Movimento da Rainha (horizontal para a esquerda usando DO-WHILE)
+    int casasRainha = 8;
+    int contadorRainha = 1;
+    printf("Movimento da Rainha (8 casas para a Esquerda):\n");
+
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    } while (contadorRainha <= casasRainha);
+
+    return 0;
+}
